6b9853a68cf1bda0107c5fb5462d41bdbb66821f
ishustov <ishustov@itcollege.ee>
2016-12-05 01:24:02 +0200

    scene1 and scene2 created

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,47 @@
 /**
  * Created by el on 29/11/2016.
  */
-public class Main {
+
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+
+public class Main extends Application {
+    Scene scene1, scene2;
+    private Sound sound;
+    public void init(){
+        Sound.music();
+    }
     public static void main(String[] args) {
-        System.out.println("Mine");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        //Button 1
+        Label label1 = new Label("Welcome to Minesweeper game! This game was made by Ilja Shustov");
+        Button button1 = new Button("Start the game");
+        button1.setOnAction(e -> stage.setScene(scene2));
+
+        //Layout 1
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(label1, button1);
+        scene1 = new Scene(layout1, 400, 400);
+
+        //Layout 2
+        StackPane layout2 = new StackPane();
+        scene2 = new Scene(layout2, 600, 300);
+
+        //Display scene 1 at first
+        stage.setScene(scene1);
+        stage.setTitle("Title Here");
+        stage.show();
+
+        stage.setScene(scene1);
+        stage.setTitle("i700");
+        stage.show();
     }
 }
